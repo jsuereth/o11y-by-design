f0d4c86e5a2b65878b7b48881a651709dfec678b
Josh Suereth <joshuasuereth@google.com>
2025-03-22 16:14:23 -0400

    Flesh out example application.

---
+++ go/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"com.jsuereth/o11y_by_design/o11y"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+)
+
+const otelName = "bid.http.server"
+
+var (
+	tracer   = otel.Tracer(otelName)
+	meter    = otel.Meter(otelName)
+	bidCount o11y.AuctionBidCount
+)
+
+func bid(w http.ResponseWriter, req *http.Request) {
+	ctx, span := tracer.Start(req.Context(), "bid")
+	defer span.End()
+	// Lookup auction id.
+	if idString := req.PathValue("id"); idString != "" {
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			w.WriteHeader(404)
+			return
+		}
+		fmt.Fprintf(w, "recording your bid\n")
+		bidCount.Add(ctx, o11y.AuctionBidCountAttributes{
+			AuctionId: id,
+		}, 1)
+		// TODO - this would actually update our bid database.
+		w.WriteHeader(200)
+	} else {
+		// Invalid request.
+		w.WriteHeader(404)
+		return
+	}
+}
+
+func init() {
+	var err error
+	bidCount, err = o11y.NewAuctionBidCount(meter)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() (err error) {
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
+	// Start HTTP server.
+	srv := &http.Server{
+		Addr:         ":8080",
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout:  time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
+
+	// Wait for interruption.
+	select {
+	case err = <-srvErr:
+		// Error when starting HTTP server.
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+
+	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
+	err = srv.Shutdown(context.Background())
+	return
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	// handleFunc is a replacement for mux.HandleFunc
+	// which enriches the handler's HTTP instrumentation with the pattern as the http.route.
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		// Configure the "http.route" for the HTTP instrumentation.
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
+	// Register handlers.
+	handleFunc("/auction/{id}/bid", bid)
+
+	// Add HTTP instrumentation for the whole server.
+	handler := otelhttp.NewHandler(mux, "/")
+	return handler
+}
